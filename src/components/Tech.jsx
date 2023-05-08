ca23bce7778c9147a3a2f39383af00d8f4f6de4d
AzharH1 <100111855+AzharH1@users.noreply.github.com>
2023-05-08 17:21:12 +1000

    Fixing bug on mobile where tech balls not showing due to overload in canvas rendering

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
+
 const Tech = () => {
+  // console.log(technologies.slice(0, Math.floor(technologies.length/2 -1)))
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
+      { isMobile ? (technologies.slice(0, Math.floor(technologies.length/2 -1))).map((technology) => (
+        TheTechBalls(technology)
+      )) : technologies.map(technology => (TheTechBalls(technology)))}
     </div>
   );
 };
+
+const TheTechBalls = technology => {
+  return (
+  <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+  </div>)
+}
 
 export default SectionWrapper(Tech, "");
